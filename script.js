b9c35d2df666389c965602bbbe5f46d0defe8ffb
Atharva <atharvakharche100@gmail.com>
2025-05-12 20:12:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let main = document.getElementById("mainId");
+
+let displayColor = document.getElementById("displayColorId")
+
+
+function onBtn1() {
+    document.getElementById("h1").textcontent="Hello"
+    main.style.backgroundColor = "#e0e0e0";
+    displayColor.textContent = "#e0e0e0";
+    
+}
+
+function onBtn2() {
+
+    main.style.backgroundColor = "#6fcf97";
+    displayColor.textContent = "#6fcf97";
+}
+
+function onBtn3() {
+
+    main.style.backgroundColor = "#56ccf2";
+    displayColor.textContent = "#56ccf2";
+}
+
+function onBtn4() {
+
+    main.style.backgroundColor = "#bb6bd9";
+    displayColor.textContent = "#bb6bd9";
+
+}
